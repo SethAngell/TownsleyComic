ff7084fc8ce0f7b7475704adae8069be653294f0
Seth Angell <accounts@sethangell.com>
2023-12-27 18:24:30 -0500

    fix: update refresh script

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 SEMANTIC_VERSION=$(curl "https://api.github.com/repos/SethAngell/TownsleyComic/tags" | jq -r '.[0].name')
+RAW_BASE_URL="https://raw.githubusercontent.com/SethAngell/TownsleyComic/main"
 
 echo Updating townsley to version $SEMANTIC_VERSION
+
+curl $RAW_BASE_URL/.ci/docker-compose.yml -o /home/Townsley/docker-compose.yml
 docker-compose pull
 docker-compose up -d --build
 
